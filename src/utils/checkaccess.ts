f151c22f18dbbe2464481bcd844f35a77252c6a6
PM <polyversialmind@gmail.com>
2020-11-05 23:06:08 -0600

    checkaccess feature

--- src/utils/checkaccess.ts
+++ src/utils/checkaccess.ts
@@ -1,13 +1,33 @@
 import xlg from '../xlogger';
-import { ExtMessage } from "../typings";
+import { ExtMessage, CheckAccessOptions } from "../typings";
 
-export = async (message: ExtMessage): Promise<boolean> => {
+export = async (message: ExtMessage, options?: CheckAccessOptions): Promise<boolean> => {
     if (!message.member) return false;
+    if (options) {
+        if (options.ownerOnly && message.author.id !== process.env.OWNERID) {
+            await message.channel.send({
+                embed: {
+                    color: process.env.FAIL_COLOR,
+                    description: `${message.member}, you do not have permission (botmaster) to use this command.`
+                }
+            }).catch(xlg.error);
+            return false;
+        }
+        if (options.adminOnly && !message.member.permissions.has("ADMINISTRATOR")) {
+            await message.channel.send({
+                embed: {
+                    color: process.env.FAIL_COLOR,
+                    description: `${message.member}, you do not have permission (admin) to use this command.`
+                }
+            }).catch(xlg.error);
+            return false;
+        }
+    }
     if (message.author.id !== process.env.OWNERID && !message.member.permissions.has("ADMINISTRATOR")) {
         await message.channel.send({
             embed: {
                 color: process.env.FAIL_COLOR,
-                description: `${message.member}, you do not have permission (admin) to use this command.`
+                description: `${message.member}, you do not have the permissions to use this command.`
             }
         }).catch(xlg.error);
         return false;
