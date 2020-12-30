94a304f8a5a62d751448e3f17594d5a5f9fca2d8
PM <polyversialmind@gmail.com>
2020-12-29 23:23:48 -0600

    added mute message

--- src/utils/counthandler.ts
+++ src/utils/counthandler.ts
@@ -1,6 +1,7 @@
 import moment from "moment";
 import { CommandClient, CountTiming, ExtMessage } from "../typings";
 import xlg from "../xlogger";
+import { getFriendlyUptime } from "./time";
 //import { handleFoul } from "./foul";
 //import fs from "fs";
 const countTimings: Array<CountTiming> = [];
@@ -256,6 +257,22 @@ async function handleMute(client: CommandClient, message: ExtMessage, offBy?: nu
         muteLength = 0.5;
     }
     const aimDate = moment(new Date()).add(muteLength, "m").toDate();
+
+    const t = getFriendlyUptime(muteLength * 60 * 1000);
+    try {
+        await message.author.send(`Hello Person Who Cannot Count 👋
+
+Your server admins have enable auto-muting.
+
+You have been muted for **${t.hours} hours, ${t.minutes} minutes, and ${t.seconds} seconds.**
+
+Check how much time remains on your mute with the \` c?ms \` command.
+
+Yours,
+Human`)
+    } catch (e) {
+        // ...just want errors to fizzle out
+    }
 
     await message.channel.updateOverwrite(message.member, {
         "SEND_MESSAGES": false
